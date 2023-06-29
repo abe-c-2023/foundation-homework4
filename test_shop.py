d70779dd736a27e6aa076e3749a7ebba0ec05cab
Abraham Clark <abk@Abrahams-Air.lan>
2023-06-29 19:57:22 +0100

    add test file

---
+++ test_shop.py
@@ -0,0 +1,24 @@
+# I have spent many hours working on this homework and am really stuck on the unit testing.
+#   I am going to submit this incomplete and have a look at fellow students' submissions to 
+#  learn about this, instead of continuing to struglle with minimal progress.
+
+
+
+# import unittest
+# from shop import shopStock, tryNbuy
+
+
+
+# class tests(unittest.TestCase):
+
+#     def test_Cactus(self):
+#         result = tryNbuy(100, "Cactus", 0)
+
+
+
+# if __name__ == '__main__':
+#     unittest.main()
+
+#     # tryNbuy(customerMoney, customerRequest, purchaseAttempts)
+
+# unittest.main()
